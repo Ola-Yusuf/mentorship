a3e7fbab21101f9c74994eef71a6a4f4c2fab785
Ola-Yusuf <olatundeyusuf90@gmail.com>
2020-11-25 15:07:08 +0100

    reseting db connect

--- api/config/Db_connect.php
+++ api/config/Db_connect.php
@@ -1,34 +1,22 @@
 <?php 
     class Db_connect {
         private $production = true;
-        private $server_name = "localhost";
-        private $db_username = "root";
-        private $db_password = "";
-        protected $db_name = "mentorship";
-        protected $url = null;
+        protected $db_url = null;
+        private $server_name =null;
+        private $db_username =null; 
+        private $db_password =null; 
+        protected $db_name =null; 
 
         public $conn;
 
         // Db connection
         public function __construct(){
-            if($production){
-                // $this->url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-                // $this->server_name = $this->url["host"];
-                // $this->db_username = $this->url["user"];
-                // $this->db_password = $this->url["pass"];
-                // $this->db_name = substr($this->url["path"], 1);
+            $this->server_name = getenv("server");
+            $this->db_username = getenv("username");
+            $this->db_password = getenv("password");
+            $this->db_name = getenv("name");
 
-                $this->server_name = getenv("server");
-                $this->db_username = getenv("username");
-                $this->db_password = getenv("password");
-                $this->db_name = getenv("name");
-            }
-            echo $this->url;
-            echo $this->server_name ;
-            echo $this->db_username ;
-            echo $this->db_password ;
-            echo $this->db_name ;
-
+            echo $this->server_name;
             $this->conn = null;
             try {
                  $this->conn = new PDO("mysql:host=$this->server_name", $this->db_username, $this->db_password);
