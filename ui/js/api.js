49ef77b8c2ed114e2da15fd4affaeb8bac3303ad
Ola-Yusuf <olatundeyusuf90@gmail.com>
2020-11-26 07:13:28 +0100

    create index page and set up profile view

--- ui/js/api.js
+++ ui/js/api.js
@@ -1,6 +1,6 @@
  class Api { 
   constructor() {
-    this.production = true;
+    this.production = false;
     if(this.production){
       this.api = "https://secure-reef-39872.herokuapp.com/api/"; //production api
     }else{
@@ -30,38 +30,97 @@
   getMentorsOfMenteeEndpoint(id) {
     return this.api+"mentor_of_mentee.php?mentee_id="+id;
     }
-  
-    async signUp(url, myForm){
-      let formdata = new FormData(myForm);
-      let request = new Request(url, { method: "POST",
-                                       body: formdata });
-      return await fetch(request);
-    }
+
 }
 
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
-  
-  let x = new Api();
-console.log(x.getMentorSignupEndpoint())
 
-const signUpForm = document.querySelector('#signUpForm')
+let x = new Api();
+const signUpForm = document.querySelector('#signUpForm'),
+       signUpButton = document.querySelector('#signUpButton'),
+       loginButton = document.querySelector('#loginButton'),
+       logoutButton = document.querySelector('#logoutButton');
 
-signUpForm.addEventListener('submit', async(e) => {
+let signUpSection = document.getElementById('signUpSection'),
+     profileSection = document.getElementById('profileSection'),
+     loginSection = document.getElementById('loginSection'),
+
+     userCategory = document.getElementById('userCategory'),
+     profile_fullname = document.getElementById('profile_fullname'),
+     profile_email = document.getElementById('profile_email'),
+     profile_created_on = document.getElementById('profile_created_on'),
+     listOfMentor = document.getElementById('listOfMentor'),
+     menteesMentor = document.getElementById('menteesMentor'),
+    password = document.getElementById("password"),
+    confirm_password = document.getElementById("password-repeat");
+
+if(sessionStorage.getItem("Token") !== null){
+  signUpSection.style.display = "none";
+  profileSection.style.display = "block";
+  loginSection.style.display = "none"
+
+  //ste profile details
+  userCategory.innerText = sessionStorage.getItem("Category");
+  profile_fullname.innerText  = sessionStorage.getItem("Name");
+  profile_email.innerText  = sessionStorage.getItem("Email");
+  profile_created_on.innerText  = sessionStorage.getItem("Created");
+
+  //check if user is mentor
+  if(sessionStorage.getItem("Category") == 'mentor'){
+    listOfMentor.style.display = "none";
+    }
+  else if(sessionStorage.getItem("Category") == 'mentee'){
+      // listOfMentor.style.display = "block";
+      menteesMentor.innerText = sessionStorage.getItem("Mentor");
+    }
+}else{
+  signUpSection.style.display = "block";
+  profileSection.style.display = "none";
+  loginSection.style.display = "none"
+}
+
+logoutButton.addEventListener('click', () => {
+    sessionStorage.clear();
+    signUpSection.style.display = "block";
+    profileSection.style.display = "none";
+    loginSection.style.display = "none";
+})
+
+loginButton.addEventListener('click', () => {
+  if(sessionStorage.getItem("Token") == true){
+    signUpSection.style.display = "none";
+    profileSection.style.display = "block";
+    loginSection.style.display = "none";
+  }else{
+    signUpSection.style.display = "none";
+    profileSection.style.display = "none";
+    loginSection.style.display = "block";
+  } 
+})
+
+signUpButton.addEventListener('click', () => {
+  if(sessionStorage.getItem("Token") == true){
+    signUpSection.style.display = "none";
+    profileSection.style.display = "block";
+    loginSection.style.display = "none";
+  }else{
+    signUpSection.style.display = "block";
+    profileSection.style.display = "none";
+    loginSection.style.display = "none";
+  } 
+})
+
+
+
+//signup for mentor and mentee
+signUpForm.addEventListener('submit', (e) => {
   e.preventDefault()
-  // await signUp(url, myForm);
+  if(password.value != confirm_password.value) {
+    alert("Password not match");
+    return null;
+  }
   let inputData = JSON.stringify(Object.fromEntries(new FormData(event.target)));
-  console.log(delete inputData['password-repeat']);
-  console.log(JSON.parse(inputData));
-  let submittable = {
-                    'name': JSON.parse(inputData).name,
-                    'email': JSON.parse(inputData).email,
-                    'password': JSON.parse(inputData).password
-                  }
-  console.log(submittable);
-
+  
   let signUpUrl = null;
   if(JSON.parse(inputData).category == 'mentor'){
      signUpUrl = x.getMentorSignupEndpoint();
@@ -69,7 +128,7 @@ signUpForm.addEventListener('submit', async(e) => {
      signUpUrl = x.getMenteeSignupEndpoint();
   }
 
-    fetch(signUpUrl, {
+  fetch(signUpUrl, {
       method: 'POST',
       body: inputData,
       headers: {
@@ -79,19 +138,58 @@ signUpForm.addEventListener('submit', async(e) => {
       if (response.ok) {
         return response.json();
       }
-      // console.log(response.status)
       return Promise.reject(response);
     }).then(function (data) {
-      console.log(data);
-      // // console.log(data.status)
+      if(data.status != 422){
+          signUpSection.style.display = "none";
+          profileSection.style.display = "block";
+          loginSection.style.display = "none"
+        //set session variable 
+          sessionStorage.setItem("Token", data.token);
+          sessionStorage.setItem("Name", data.profile.name);
+          sessionStorage.setItem("Email", data.profile.email);
+          sessionStorage.setItem("Created", data.profile.created);
+          sessionStorage.setItem("Category", data.category);
 
-    }).catch(function (error) {
-      console.warn(error);
-      // console.log(error.status)
+          userCategory.innerText = data.category;
+          profile_fullname.innerText  = data.profile.name;
+          profile_email.innerText  = data.profile.email;
+          profile_created_on.innerText  = data.profile.created;
+          if(data.category == 'mentor'){
+            listOfMentor.style.display = "none";
+          }else{
+          // sessionStorage.setItem("User", 'mentee');
+          sessionStorage.setItem("Mentee_id", data.profile.mentee_id);
+          
+          for (let index = 0; index < data.mentors.length; index++) {
+            if(menteesMentor.innerText !== "None"){
+              menteesMentor.innerText = menteesMentor.innerText + ', '+ data.mentors[index].mentor_name ;
+            }else{
+              menteesMentor.innerText = data.mentors[index].mentor_name; 
+            }       
+          }
+          sessionStorage.setItem("Mentor", menteesMentor.innerText);
+        }
+        alert(data.message);
+      }else{
+        // (data.error)
+    sessionStorage.clear();
 
-    });
-  
-})
+        alert(data.error);
+      }
+    }).catch(function (error)  {
+      alert(error);
+    }); 
+
+});
+
+//login for mentor and mentee
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  alert("Not Implemented");
+});
+
+
 
 
 
--- ui/js/app.js
+++
@@ -1,2 +0,0 @@
-import Api from './api';
-console.log((new Api).getDatabaseSetupEndPoint());
